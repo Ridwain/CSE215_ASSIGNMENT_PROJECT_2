f710c91c3d98eb4857ed79d577aaa876fe703bf6
Ridwain <ridwain.islam@northsouth.edu>
2023-10-12 03:39:38 +0600

    Update CourseManagement.java
    ok 2nd

--- CSE_ASSIGN_2/src/CourseManagement.java
+++ CSE_ASSIGN_2/src/CourseManagement.java
@@ -1,7 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class CourseManagement {
     public static void main(String[] args) {
+        while(true) {
+            Session session = Session.getSession();
+
+            //User userList[] =session.getUserList();
+            Course courseList[] = session.getCourseList();
+            System.out.println(courseList[2].getCourseName());
+            break;
+        }
+
+        //kllll
+        //hhu
 
     }
 }
